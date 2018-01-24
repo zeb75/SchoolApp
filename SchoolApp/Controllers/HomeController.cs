0e7f5359278990ea35afa34c47f4bad187e606a2
Fredrik Lundqvist <zeb75@hotmail.com>
2018-01-24 16:12:55 +0100

    v1.1

---
+++ SchoolApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using SchoolApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolApp.Controllers
+{
+    public class HomeController : Controller
+    { 
+         SchoolAppDbContext db = new SchoolAppDbContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+    }
+}
